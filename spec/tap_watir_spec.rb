7a1257718c6bb03e66e42c784694d9e0d1b32553
Prakhar Rawat <pxr7185@g.rit.edu>
2018-07-03 11:53:33 -0700

    updated code in test branch

--- spec/tap_watir_spec.rb
+++ spec/tap_watir_spec.rb
@@ -11,4 +11,32 @@ RSpec.describe TapWatir do
     expect(browser.url).to eq "http://watir.com/"
     browser.close
   end
+
+
+
+
+
+  RSpec.describe TapWatir do
+    it 'should be able to open android application notes and do stuff' do
+      appium_url = 'http://localhost:4723/wd/hub'
+      caps = {platformName: 'Android',
+              platformVersion: '8.1',
+              deviceName: 'Nexus',
+              app: '/Users/prakharrawat/Document/notes.apk'
+
+      }
+driver = TapWatir::App.browser(url:appium_url,desired_capabilities:caps)
+      driver.element(xpath: '/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.support.v4.widget.DrawerLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout[2]/android.widget.Button
+').click()
+
+      driver.element(xpath: '/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.EditText
+').send_keys("This is the title of the note")
+
+      driver.element(xpath: '/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.EditText
+').send_keys "This is the body of the note"
+
+      # driver.quit
+
+    end
+  end
 end
