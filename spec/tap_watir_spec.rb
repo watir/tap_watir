7afbc025e1e0257cbaa37d49d043b9f5c54e91a0
Prakhar Rawat <pxr7185@g.rit.edu>
2018-07-11 18:04:52 -0700

    hybrid app opens in emu - tested with ebay's application

--- spec/tap_watir_spec.rb
+++ spec/tap_watir_spec.rb
@@ -1,3 +1,10 @@
+require 'rspec/expectations'
+require 'appium_lib_core'
+require 'rspec'
+
+
+
+
 RSpec.describe TapWatir do
   context "with Android" do
     it "opens Browser on Mobile Device" do
@@ -6,7 +13,8 @@ RSpec.describe TapWatir do
               platformName: 'Android',
               platformVersion: '9',
               deviceName: 'Nexus',
-              browserName: 'Chrome'}
+              browserName: 'Chrome'
+      }
 
       browser = TapWatir::MobileBrowser.new(url:opts[:url],desired_capabilities:opts)
       browser.goto "http://watir.com/"
@@ -28,21 +36,31 @@ RSpec.describe TapWatir do
       app.quit
     end
 
-    it "opens Native App EmuSim" do
-      opts = {
-          url: "http://ondemand.saucelabs.com:443/wd/hub",
-          platformName: "Android",
-          platformVersion: "7.1",
-          deviceName: "Android GoogleAPI Emulator",
-          app: "sauce-storage:eb.apk",
-          sauce_username: "prakhar.rawat"
 
-      }
+      it "opens Native App EmuSim" do
+        opts = {
 
-      app = TapWatir::MobileBrowser.new(url:opts[:url],desired_capabilities:opts)
-      app.wait 2
-      app.quit
-    end
+            platformName: "Android",
+            platformVersion: "7.1",
+            deviceName: "Android GoogleAPI Emulator",
+            app: "sauce-storage:eb.apk",
+            buildName: "Hybrid application test using emu/sim",
+            url: "user your own sauce credentials dude ",
+            sauce_username: ENV['SAUCE_LABS'] ? ENV['SAUCE_USERNAME'] : nil,
+            sauce_access_key: ENV['SAUCE_LABS'] ? ENV['SAUCE_ACCESS_KEY'] : nil
+        }
+
+        newUrl = opts.delete(:url)
+        app = TapWatir::MobileBrowser.new(url:newUrl,desired_capabilities:opts)
+        app.wait 2
+        app.close
+      end
+
+
+
+
+
+
     xit "opens Native App Real Device Cloud"
   end
 
