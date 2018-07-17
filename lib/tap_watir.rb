5578af97c0a5c0edc6825887cf81b20c8aa85a8b
Titus Fortner <titusfortner@gmail.com>
2018-07-16 18:44:16 -0700

    implements opening Android applications

--- lib/tap_watir.rb
+++ lib/tap_watir.rb
@@ -1,15 +1,27 @@
 require "watir"
+require 'appium_lib_core'
+require 'appium/driver'
 
 module TapWatir
   class App
-    def self.browser(opts)
-      MobileBrowser.new opts
+
+    attr_accessor :driver
+
+    def initialize(opts)
+      url = opts[:caps].delete(:url)
+      @driver = Appium::Core.for(self, opts).start_driver(server_url: url)
     end
+
+    def quit
+      @driver.quit
+    end
+    alias_method :close, :quit
+
   end
 
   class MobileBrowser < Watir::Browser
     def initialize(opts)
-      super Selenium::WebDriver.for(:remote, opts)
+      @browser = super Selenium::WebDriver.for(:remote, opts)
     end
   end
 end
