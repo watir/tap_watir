ff5831de57e62cc6b72706e81c3bcb48d3d72e9f
Prakhar Rawat <pxr7185@g.rit.edu>
2018-07-24 10:18:51 -0700

    implements element class

---
+++ lib/tap_watir/element.rb
@@ -0,0 +1,5 @@
+module TapWatir
+  class Element
+
+  end
+end
--- spec/tap_watir_spec.rb
+++ spec/tap_watir_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
-RSpec.describe TapWatir do
+RSpec.describe TapWatir::App do
   it "delegates calls to driver" do
     expect($app.respond_to? :session_capabilities) #responds to it
     expect($app.session_capabilities).to be_a Selenium::WebDriver::Remote::W3C::Capabilities # match with the seleniums caps
 
   end
 
+  describe "#element" do
+    it 'returns a TapWatir Element' do
+      expect($app.element).to be_a TapWatir::Element #evaluates the instance of tap watir element
+    end
+  end
 end
