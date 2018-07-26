ca03d17641c002d11a961ad33374698f4fae03ae
Prakhar Rawat <pxr7185@g.rit.edu>
2018-07-26 09:07:55 -0700

    implements exist and fixes PR #7

--- lib/tap_watir/element.rb
+++ lib/tap_watir/element.rb
@@ -1,3 +1,5 @@
+require 'watir'
+
 module TapWatir
   class Element
 
@@ -6,13 +8,27 @@ module TapWatir
       @selector = selector
     end
 
-    # should be made private to limit access to assert_exist?
+    # should be private
+    private
     def locate
       @element = @driver.find_element(@selector.keys.first,@selector.values.first)
     end
 
     def wd
       @element || locate
+    end
+
+    def exist?
+
+      return false if @element
+      assert_exists
+    end
+
+
+    def assert_exists? # should I be using rescue ??
+      locate unless @element
+      return if @element
+      raise Watir::Exception::UnknownObjectException
     end
 
   end
